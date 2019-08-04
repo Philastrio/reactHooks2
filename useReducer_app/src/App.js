dfe0702d4d6504feb6939f2e2a22bd0f06e7ff10
philastrio <philastrio@gmail.com>
2019-08-04 09:07:17 +0900

    9-10, 1:30할차례

--- useReducer_app/src/App.js
+++ useReducer_app/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import reducer, { initialState, ADD, DEL } from "./reducer";
 
-function App() {
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newTodo, setNewTodo] = useState("");
+
+  const onSubmit = e => {
+    e.preventDefault();
+    // 새로고침 하지말라는 것으로, 쉽게 말해 아무것도 안하겠다는 의미이다.
+    dispatch({ type: ADD, payload: newTodo });
+  };
+
+  const onChange = e => {
+    const {
+      target: { value }
+    } = e;
+    setNewTodo(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>오늘의 할일 목록</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newTodo}
+          type="text"
+          placeholder="할일 추가"
+          onChange={onChange}
+        />
+      </form>
+      <ul>
+        <h2>할일 목록</h2>
+        {state.toDos.map((toDo, index) => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+              삭제
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
---
+++ useReducer_app/src/reducer.js
@@ -0,0 +1,33 @@
+import uuid from "uuid/v4";
+
+export const initialState = {
+  toDos: []
+};
+
+export const ADD = "add";
+export const DEL = "del";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      return {
+        toDos: [...state.toDos, { id: uuid(), text: action.payload }]
+      };
+    case DEL:
+      return {
+        toDos: state.toDos.filter(toDo => {
+          console.log("Current: ", toDo.id, "Target: ", action.payload);
+          return toDo.id !== action.payload;
+        })
+      };
+    // ...state.toDos: toDos가 가지고 있던 내용물을 의미한다
+    // return {toDos: [{text: action.payload}]} : 단 하나의 toDo만을 가진 array를 얻게 된다는 의미이다
+    // push를 쓰면 안되나? push는 배열을 변화시키니 버그가 난다. 우린 array를 변화시키지 않는다. 새로운 것으로
+    // '대체'할 뿐이다. 이전의 toDos르 가지고, 여기에 새로운 toDo를 더한다. 이를 anti mutation이라 한다
+    // 여기서는 변화를 허용하지 않는다. 대체할 뿐이다
+    default:
+      return;
+  }
+};
+
+export default reducer;
